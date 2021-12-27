b2b82799549cc4b1003a9c691ee55f00538d9699
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-12-27 18:09:46 +0100

    fix: create the file for checking commits

--- src/main/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsExtension.kt
+++ src/main/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsExtension.kt
@@ -1,5 +1,15 @@
 package it.nicolasfarabegoli.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.provider.ListProperty
+import org.gradle.kotlin.dsl.listProperty
 
-open class ConventionalCommitsExtension(private val project: Project)
+open class ConventionalCommitsExtension(project: Project) {
+
+    /**
+     * List of scopes admitted in the project.
+     * If an empty list is given, all scopes are admitted.
+     * By default, any scope is admitted.
+     */
+    val scopes: ListProperty<String> = project.objects.listProperty()
+}
