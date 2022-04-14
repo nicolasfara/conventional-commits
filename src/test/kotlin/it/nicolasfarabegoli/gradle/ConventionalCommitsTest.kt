a2b5c82ba37cd772c284736e4cc35f32cfdd599f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-04-14 12:57:10 +0200

    feat!: add feature for creating the script from remote URL or directly from string

--- src/test/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsTest.kt
+++ src/test/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsTest.kt
@@ -11,7 +11,7 @@ import java.io.File
  */
 class ConventionalCommitsTest : WordSpec({
     val projectDir = File("build/gradleTest")
-    fun setupTest() {
+    fun setupDefaultTest() {
         projectDir.mkdirs()
         projectDir.resolve("settings.gradle.kts").writeText("")
         projectDir.resolve("build.gradle.kts").writeText(
@@ -22,17 +22,60 @@ class ConventionalCommitsTest : WordSpec({
             """.trimIndent()
         )
     }
+    fun setupCustomTest() {
+        projectDir.mkdirs()
+        projectDir.resolve("settings.gradle.kts").writeText("")
+        projectDir.resolve("build.gradle.kts").writeText(
+            """
+            plugins {
+                id("it.nicolasfarabegoli.conventional-commits")
+            }
+            
+            conventionalCommits {
+                from {
+                    "echo \"Hello World\""
+                }
+                setupScript()
+            }
+            """.trimIndent()
+        )
+    }
 
-    "The plugin" should {
-        "create a script file" {
-            setupTest()
-            val tasks = GradleRunner.create()
-                .forwardOutput()
-                .withPluginClasspath()
-                .withProjectDir(projectDir)
-                .withArguments("tasks")
-                .build()
-            tasks.task(":tasks")?.outcome shouldBe TaskOutcome.SUCCESS
+    "The plugin" `when` {
+        "applied without configurations" should {
+            "generate the default script in `.git/hooks/commit-msg`" {
+                setupDefaultTest()
+                val tasks = GradleRunner.create()
+                    .forwardOutput()
+                    .withPluginClasspath()
+                    .withProjectDir(projectDir)
+                    .withArguments("tasks")
+                    .build()
+                tasks.task(":tasks")?.outcome shouldBe TaskOutcome.SUCCESS
+
+                val scriptFile = File(projectDir.absolutePath, "/.git/hooks/commit-msg")
+                scriptFile.exists() shouldBe true
+                scriptFile.readText() shouldBe Thread.currentThread()
+                    .contextClassLoader
+                    .getResource("it/nicolasfarabegoli/gradle/commit-msg.sh")
+                    .readText()
+            }
+        }
+        "applied with a configuration with `from` rule" should {
+            "generate the script with the content given by the user" {
+                setupCustomTest()
+                val tasks = GradleRunner.create()
+                    .forwardOutput()
+                    .withPluginClasspath()
+                    .withProjectDir(projectDir)
+                    .withArguments("tasks")
+                    .build()
+                tasks.task(":tasks")?.outcome shouldBe TaskOutcome.SUCCESS
+
+                val scriptFile = File(projectDir.absolutePath, "/.git/hooks/commit-msg")
+                scriptFile.exists() shouldBe true
+                scriptFile.readText() shouldBe "#!/usr/bin/env bash\n\necho \"Hello World\""
+            }
         }
     }
 })
