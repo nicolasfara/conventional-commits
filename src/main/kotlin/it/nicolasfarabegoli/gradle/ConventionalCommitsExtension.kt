a2e5162044aaaee5dfd78596a9a86f673ae04fcf
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-06-14 23:03:32 +0200

    docs: add kotlin doc to plugin properties

--- src/main/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsExtension.kt
+++ src/main/kotlin/it/nicolasfarabegoli/gradle/ConventionalCommitsExtension.kt
@@ -8,10 +8,47 @@ import org.gradle.api.Project
 open class ConventionalCommitsExtension(private val project: Project) {
 
     var warningIfNoGitRoot: Boolean = true
-    var types: List<String> = emptyList()
+
+    /**
+     * List of admitted types.
+     *
+     * Default values are: _build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test_.
+     */
+    var types: List<String> = listOf(
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+    )
+
+    /**
+     * List of admitted scopes.
+     *
+     * By default an empty list is set, meaning that all scopes are admitted.
+     */
     var scopes: List<String> = emptyList()
-    var successMessage: String? = null
-    var failureMessage: String? = null
+
+    /**
+     * The message that will be shown if the commit is conventional commit compliant.
+     */
+    var successMessage: String? = "\\e[32mCommit message meets Conventional Commit standards...\\e[0m"
+
+    /**
+     * The message that will be shown if the commit is not conventional commit compliant.
+     */
+    var failureMessage: String? = """
+        \e[31mThe commit message does not meet the Conventional Commit standard\e[0m
+        An example of a valid message is: 
+          feat(login): add the 'remember me' button
+        More details at: https://www.conventionalcommits.org/en/v1.0.0/#summary
+    """.trimIndent()
 
     /**
      * This method do all the work needed to write the script file.
