90f55f389a9e5397c6aca40616940e5871fbb032
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-06-15 09:57:27 +0200

    docs: add some other kotlin doc

--- src/test/kotlin/it/nicolasfarabegoli/gradle/Utils.kt
+++ src/test/kotlin/it/nicolasfarabegoli/gradle/Utils.kt
@@ -2,6 +2,12 @@ package it.nicolasfarabegoli.gradle
 
 import java.io.File
 
+/**
+ * Utility function for writing the content of `build.gradle.kts` with a string provider.
+ */
 fun File.configureBuildGradle(content: () -> String) = this.resolve("build.gradle.kts").writeText(content())
 
+/**
+ * Utility function for writing the content of `settings.gradle.kts` with a string provider.
+ */
 fun File.configureSettingsGradle(content: () -> String) = this.resolve("settings.gradle.kts").writeText(content())
